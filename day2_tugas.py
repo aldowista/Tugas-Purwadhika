a12c27200bdf574eeee6888935e6fe459365afce
aldowista <fadhilah.aldo123@gmail.com>
2020-08-25 15:30:10 +0700

    Tugas Purwadhika Hari Ke-2

---
+++ day2_tugas.py
@@ -0,0 +1,48 @@
+# #Tugas 1
+# print('Tugas 1')
+# masukanTugas1 = input('Masukan 4 digit angka: ')
+
+# a = masukanTugas1[0]
+# b = masukanTugas1[1]
+# c = masukanTugas1[2]
+# d = masukanTugas1[3]
+
+# print(f'Input : {masukanTugas1}')
+# print(f'Output : {c}{d}{a}{b}')
+
+#Tugas 2
+# import math
+# print('Tugas 2')
+# masukanTugas2= int(input('Masukan nilai jari2 lingkaran: '))
+# outputTugas2= math.pi * math.pow(masukanTugas2,2)
+
+# print(f'Input Radius : {masukanTugas2}')
+# print(f'Output Area : {outputTugas2}')
+
+# #Tugas 3
+# print('Tugas 3')
+# masukanTugas3a= (input('Masukan 2 digit angka pertama: '))
+# masukanTugas3b= (input('Masukan 2 digit angka kedua: '))
+
+# print(f'Input pertama : {masukanTugas3a}')
+# print(f'Input kedua : {masukanTugas3b}')
+
+# out31a = masukanTugas3a[0]
+# out32a = masukanTugas3a[1]
+# out31b = masukanTugas3b[0]
+# out32b = masukanTugas3b[1]
+
+# print(f'Output : {out31a}{out31b}{out32a}{out32b}')
+
+# #Tugas 4 
+# print('Tugas 4')
+# masukanTugas4a = input('Masukan string : ')
+# masukanTugas4b = input('Masukan huruf/karakter yang akan dihilangkan: ')
+# print(f'Input: {masukanTugas4a}')
+# print(f'Output: {masukanTugas4a.split(masukanTugas4b)} ')
+
+# #Tugas 5
+
+# masukanTugas5a = input('Masukan inputan 2 kata : ')
+# print(f'Input : {masukanTugas5a}')
+# print(f'Output : {masukanTugas5a.split(" ")[1]} {masukanTugas5a.split(" ")[0]}')
